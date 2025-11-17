27007007a92d44a4e79643f028fad0786e1f020a
NDT28092005 <ndaitung28092005@gmail.com>
2025-11-17 15:12:29 +0700

    fix

--- src/components/admin/ProductReviewManagement.jsx
+++ src/components/admin/ProductReviewManagement.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axiosInstance from "../../../api/axios";
+import axiosInstance from "../../api/axios";
 import { useParams } from "react-router-dom";
 
 export default function ProductReviewManagement() {
